5830cf17a0bf4268618edde1dcd9049416f3cb5e
Wes Cano <wesacano@gmail.com>
2019-09-25 11:47:05 -0500

    Updates to use MongoDB Atlas in connection string

--- app.js
+++ app.js
@@ -14,9 +14,17 @@ var express = require("express"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://yelpCampDBUser:ybSLwLoYJ5HOOyEI@cluster0-eovw2.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("Connected to DB");
+}).catch(err => {
+    console.log("ERROR: "+err);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
